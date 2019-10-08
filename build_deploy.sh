fbfcea8319c38c2d8017cc53cd4116742d89cf9a
Hieu Tran <tr.kimhieu@gmail.com>
2019-10-09 00:50:59 +0700

    Allow docker login

--- build_deploy.sh
+++ build_deploy.sh
@@ -14,7 +14,7 @@ echo "SDKS = $SDKS"
 echo "LATEST_SDKS = $LATEST_SDKS"
 echo "LATEST_PACKAGES = $LATEST_PACKAGES"
 
-# docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
 build_deploy_minimal() {
     echo "Building 'minimal' image…"
