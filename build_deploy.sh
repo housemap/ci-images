ee63ceb59e49bb6f84558aab9bd0202bdf702e13
Hieu Tran <tr.kimhieu@gmail.com>
2019-10-08 21:34:11 +0700

    Add build script and travis

---
+++ build_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# abort on errors and on unset variables
+set -e -o nounset
+
+MINIMAL_IMAGE=housemap/ci:minimal
+MINIMAL_DOCKERFILE=Dockerfile.minimal
+
+SDKS=$(echo {29..22})
+LATEST_SDKS=$(echo {29..27})
+LATEST_PACKAGES=''; for SDK in $LATEST_SDKS; do LATEST_PACKAGES="platforms;android-${SDK} $LATEST_PACKAGES"; done
+
+echo "SDKS = $SDKS"
+echo "LATEST_SDKS = $LATEST_SDKS"
+echo "LATEST_PACKAGES = $LATEST_PACKAGES"
+
+# docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+
+build_deploy_minimal() {
+    echo "Building 'minimal' image…"
+    docker build --tag $MINIMAL_IMAGE --file $MINIMAL_DOCKERFILE .
+    docker push $MINIMAL_IMAGE
+    echo
+}
+
+build_deploy_minimal
